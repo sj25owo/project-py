81bf197ad7a3574a3fc86dbd082fc6c56d05ebef
shk0505 <shk05year@gmail.com>
2025-05-30 12:11:51 +0900

    Update callapi.py
    설명 추가

--- musicpkg/callapi.py
+++ musicpkg/callapi.py
@@ -1,7 +1,7 @@
 def verify_spotipy() :
     try:
         import spotipy
-    except ImportError:
+    except ImportError: # 스포티파이 API import 해보고 설치가 안 되어있을 경우 설치 후 import
         import subprocess
         import sys
         subprocess.check_call([sys.executable, "-m", "pip", "install", "spotipy"])
