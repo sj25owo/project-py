d5617a3e991d83f55184a762016b5b661b9954cc
sj25owo <yu03471a@gmail.com>
2025-06-06 15:17:58 +0900

    Update TEST_module.py
    오류 코드 확인 후 수정 (다른 글자 입력 시 오류 발생)

--- testpkg/TEST_module.py
+++ testpkg/TEST_module.py
@@ -1,5 +1,14 @@
+def determine_input(question):
+    while True:
+        answer = input(question).strip().upper()
+        if answer in ["Y", "N"]:
+            return answer
+        else:
+            print("Y 또는 N을 입력해주세요.")
+
+
 def music_test() :
-    print ("당신의 취향에 맞는 노래를 추천해드려요! 질문에 맞게 Y/N 로 대답해주세요!")
+    print ("안녕하세요:) 테스트 중심 음악 추천 프로그램 입니다. \n당신의 취향에 맞는 노래를 추천해드려요! \n질문에 맞게 Y/N 로 대답해주세요!")
 
     genre_result = "" #  api에 적용하기 위해서 변수 생성
 
@@ -8,15 +17,15 @@ def music_test() :
     # 2. strip() 은 공백제거 3. upper은 대문자로 변환
     
     if A1 == "Y" :
-        A2 = input("리듬감 있는 음악을 선호하나요? (Y/N)").strip().upper() 
+        A2 = determine_input("리듬감 있는 음악을 선호하나요? (Y/N)").strip().upper() 
         if A2 == "Y" :
-            A3 = input("아이돌 스타일의 음악을 선호하나요? (Y/N)").strip().upper() 
+            A3 = determine_input("아이돌 스타일의 음악을 선호하나요? (Y/N)").strip().upper() 
             if A3 == "Y" :
                 result = "k-pop" #최종 결과를 result에 넣어서 result를 메인으로 부르기!
             else:
-                A4 = input("밴드사운드를 선호하나요? (Y/N)").strip().upper()
+                A4 = determine_input("밴드사운드를 선호하나요? (Y/N)").strip().upper()
                 if A4 == "Y" :
-                    A5 = input("랩처럼 리듬감있는 음악을 선호하나요? (Y/N)").strip().upper()
+                    A5 = determine_input("랩처럼 리듬감있는 음악을 선호하나요? (Y/N)").strip().upper()
                     if A5 == "Y" :
                         result = "hip-hop"
                     else :
@@ -24,18 +33,18 @@ def music_test() :
                 else : 
                     result = "dance pop"
         else :
-            A6 = input("부드러운 목소리의 음악을 선호하나요? (Y/N)").strip().upper()
+            A6 = determine_input("부드러운 목소리의 음악을 선호하나요? (Y/N)").strip().upper()
             if A6 == "Y" :
                 result = "indie pop"
             else : result = " pop"
     elif A1 == "N" :
-        A7 = input("가사를 중요하게 생각하나요? (Y/N)").strip().upper()
+        A7 = determine_input("가사를 중요하게 생각하나요? (Y/N)").strip().upper()
         if A7 == "Y" :
             result = "ballad"
         else :
-            A8 = input("악기 연주를 선호하나요? (Y/N)").strip().upper()
+            A8 = determine_input("악기 연주를 선호하나요? (Y/N)").strip().upper()
             if A8 == "Y":
-                A9 = input("클래식 연주를 선호하나요? (Y/N)").strip().upper()
+                A9 = determine_input("클래식 연주를 선호하나요? (Y/N)").strip().upper()
                 if A9 == "Y" :
                     result = "classical"
                 else : 
